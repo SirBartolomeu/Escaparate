893e1229fbb616eb11d3f68f3026861dc2ddb3a3
Henrique Saura <henrique_saura@unifei.edu.br>
2021-03-26 11:09:46 -0300

    App Component, contato, projetos

--- portme-view/src/app/app.module.ts
+++ portme-view/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ProjetosComponent,
+    ContatoComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
